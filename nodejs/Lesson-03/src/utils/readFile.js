7fcacade6142ba112e6e86a9d2b7228031c0fb2f
muradorucov <murad.po@code.edu.az>
2026-05-09 21:20:47 +0400

    bug fixxxxed

--- nodejs/Lesson-03/src/utils/readFile.js
+++ nodejs/Lesson-03/src/utils/readFile.js
@@ -1,14 +1,9 @@
 const fs = require("fs");
-const path = require("path");
 
 const readFileUtil = (dbname) => {
-  const dbPath = path.join(process.cwd(), "db.json");
+  const data = JSON.parse(fs.readFileSync("./db.json", "utf-8"))
+  return dbname ? data[dbname] : data
+}
 
-  const data = JSON.parse(
-    fs.readFileSync(dbPath, "utf-8")
-  );
-
-  return dbname ? data[dbname] : data;
-};
 
 module.exports = readFileUtil;
--- nodejs/Lesson-03/src/utils/writeFile.js
+++ nodejs/Lesson-03/src/utils/writeFile.js
@@ -1,13 +1,8 @@
 const fs = require("fs");
-const path = require("path");
 
 const writeFileUtil = (data) => {
-  const dbPath = path.join(process.cwd(), "db.json");
+  fs.writeFileSync("./db.json", JSON.stringify(data, null, 2))
+}
 
-  fs.writeFileSync(
-    dbPath,
-    JSON.stringify(data, null, 2)
-  );
-};
 
 module.exports = writeFileUtil;
