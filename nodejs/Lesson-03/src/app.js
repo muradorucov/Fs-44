cb27933ac15086da8401944e578c1a6ef1ed1289
muradorucov <murad.po@code.edu.az>
2026-05-19 20:03:27 +0400

    feat: test

--- nodejs/Lesson-03/src/app.js
+++ nodejs/Lesson-03/src/app.js
@@ -2,11 +2,37 @@ const express = require("express")
 const app = express();
 const router = require("./routers");
 const cors = require("cors")
+require("dotenv").config()
 
 
-app.use(cors())
+// const allowedOrigins = [
+//   origin1,
+//   orogin2
+// ]
+
+
+app.use(cors(
+  {
+    origin: function (origin, callback) {
+      console.log(origin);
+
+      if (!allowedOrigins.includes(origin)) {
+        const msg = "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  }
+))
 app.use(express.json());
 
 app.use("/", router)
 
 module.exports = app;
+
+
+// origin
+
+// cors access
+// https://front.az
